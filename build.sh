bf8a01f533b781da4337c7be754b67d0c09dbea8
tiechui1994 <2904951429@qq.com>
2021-04-07 17:24:31 +0800

    add upload

--- build.sh
+++ build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
 # Versions to use.
-CODE=2021a
-DATA=2021a
+CODE="$1"
+DATA="$1"
 
 set -e
+if [[ -z ${GITHUB_WORKSPACE} ]]; then
+    GITHUB_WORKSPACE=$(pwd)
+fi
+
 rm -rf work && mkdir work && cd work && mkdir zoneinfo
 curl -L -O https://www.iana.org/time-zones/repository/releases/tzcode${CODE}.tar.gz
 curl -L -O https://www.iana.org/time-zones/repository/releases/tzdata${DATA}.tar.gz
@@ -13,8 +17,8 @@ tar xzf tzdata${DATA}.tar.gz
 
 make CFLAGS=-DSTD_INSPIRED AWK=awk TZDIR=zoneinfo posix_only
 
-cd zoneinfo && zip -0 -r zoneinfo.zip *
-cd ../../
+cd zoneinfo && zip -0 -r zoneinfo.zip * && mv zoneinfo.zip ${GITHUB_WORKSPACE}/${CODE}_zoneinfo.zip
+cd ${GITHUB_WORKSPACE}
 
 echo
 if [[ "$1" = "-work" ]]; then
@@ -23,4 +27,4 @@ else
 	rm -rf work
 fi
 
-echo New time zone files in zoneinfo.zip.
+echo "New time zone files in ${GITHUB_WORKSPACE}/${CODE}_zoneinfo.zip"
